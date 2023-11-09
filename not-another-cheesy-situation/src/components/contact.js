447a8113f95c3baae71c114bb841911807b12c1b
Megan Huckabay <huckabay90@gmail.com>
2023-11-09 14:57:46 -0800

    added contact section

---
+++ not-another-cheesy-situation/src/components/contact.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+function Contact () {
+    return (
+        <div class="Contact">
+        <h3>Contacts</h3>
+
+        <div>
+            <a id="phone" href="tel:559-793-5010">559-793-5010</a>
+            <a id="email" href="mailto:TechChick2023@gmail.com">techChick2023@gmail.com</a>
+        </div>
+    </div> 
+    )
+}
+
+export default Contact;
